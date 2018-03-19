06616b6fbcc1d1c63a398fe0d151ea434d0a4457
aw <aw@yha.com>
2018-03-18 21:50:32 -0400

    last part added prior to checking on a real device

--- app/src/main/java/com/example/amanda/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/amanda/geoquiz/QuizActivity.java
@@ -36,20 +36,14 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
-                // Does nothing yet, but soon!
+                checkAnswer(true);
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
-                // Does nothing yet, but soon!
+                checkAnswer(false);
             }
         });
         mNextButton = (Button) findViewById(R.id.next_button);
@@ -66,7 +60,7 @@ public class QuizActivity extends AppCompatActivity {
 
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
-        mQuestionTtView.setText(question);
+        mQuestionTextView.setText(question);
     }
     private void checkAnswer(boolean userPressedTrue) {
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
